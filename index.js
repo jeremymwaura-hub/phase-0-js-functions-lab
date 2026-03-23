766279bc0f12dbdac367137c7daf02520d3a8ac6
Jeremy Mwaura <jeremy.mwaura@student.moringaschool.com>
2026-03-23 11:08:29 +0300

    first

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
