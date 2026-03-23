b73c7663abdf7e166b8d776d4778783fcd0bcf19
Jeremy Mwaura <jeremy.mwaura@student.moringaschool.com>
2026-03-23 11:15:42 +0300

    second

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+//calculateTax
+function calculateTax(amount, taxRate) {
+    return amount * (taxRate / 100);
+}
+
+//convertToUpperCase
+function convertToUpperCase(str) {
+    return str.toUpperCase();
+}
+
+//findMaximum
+function findMaximum(arr) {
+    return Math.max(...arr);
+}
+
+//isPalindrome
+function isPalindrome(str) {
+    const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+
+//calculateDiscountedPrice
+function calculateDiscountedPrice(price, discount) {
+    return price - (price * (discount / 100));
+}
+
 
 
 
