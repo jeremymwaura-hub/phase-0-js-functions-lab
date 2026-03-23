ceda348532414b424d01c0ff4206382948d1d56c
Jeremy Mwaura <jeremy.mwaura@student.moringaschool.com>
2026-03-23 11:33:51 +0300

    fifth

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 // Function 1: calculateTax
 function calculateTax(amount) {
-  let tax = amount * 0.10;
-  return tax;
+  return amount * 0.10;
 }
-
 
 // Function 2: convertToUpperCase
 function convertToUpperCase(text) {
   return text.toUpperCase();
 }
-
 
 // Function 3: findMaximum
 function findMaximum(num1, num2) {
@@ -20,23 +17,24 @@ function findMaximum(num1, num2) {
   }
 }
 
-
 // Function 4: isPalindrome
 function isPalindrome(word) {
   let reversed = word.split("").reverse().join("");
-
-  if (word === reversed) {
-    return true;
-  } else {
-    return false;
-  }
+  return word === reversed;
 }
-
 
 // Function 5: calculateDiscountedPrice
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
   let discount = (discountPercentage / 100) * originalPrice;
-  let finalPrice = originalPrice - discount;
-
-  return finalPrice;
+  return originalPrice - discount;
 }
+
+
+// EXPORTING THE FUNCTIONS
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
